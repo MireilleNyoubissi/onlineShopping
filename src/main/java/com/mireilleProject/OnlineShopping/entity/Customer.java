c282e423655b9e94f641f61b983814894c57170c
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-06-27 22:29:08 +0100

    Added the relationship Onetomany with Subscriptions

--- src/main/java/com/mireilleProject/OnlineShopping/entity/Customer.java
+++ src/main/java/com/mireilleProject/OnlineShopping/entity/Customer.java
@@ -1,12 +1,16 @@
 package com.mireilleProject.OnlineShopping.entity;
 
 import com.mireilleProject.OnlineShopping.models.User;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.Set;
 
 @Entity
 @Data
 @Table(name = "CUSTOMER")
 public class Customer extends User {
+
+    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<Subscriptions> subscriptions;
 }
