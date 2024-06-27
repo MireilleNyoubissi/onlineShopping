181bb51cdca2cbecb4a294cf98fd0c9aa53783a7
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-06-27 21:56:50 +0100

    Updated Userdto with firstName and lastName attributes

--- src/main/java/com/mireilleProject/OnlineShopping/dto/UserDto.java
+++ src/main/java/com/mireilleProject/OnlineShopping/dto/UserDto.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class UserDto {
-    private String name;
+    private String firstName;
+    private String lastName;
     private String email;
-    private String phone;
 }
