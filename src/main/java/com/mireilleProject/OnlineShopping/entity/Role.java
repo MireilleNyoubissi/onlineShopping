eaf25b948abbae1c82c06ea360b26f1fcff4ca0a
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-07-16 09:54:02 +0100

    Added role entity

--- src/main/java/com/mireilleProject/OnlineShopping/entity/Role.java
+++ src/main/java/com/mireilleProject/OnlineShopping/entity/Role.java
@@ -1,4 +1,21 @@
 package com.mireilleProject.OnlineShopping.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ROLES")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
 }
