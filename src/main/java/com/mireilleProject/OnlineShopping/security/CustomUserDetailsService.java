9248311dcc12fcf65a86b5bbfe45beb1c422c632
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-07-16 09:59:48 +0100

    Added CustomUserDetailsService

--- src/main/java/com/mireilleProject/OnlineShopping/security/CustomUserDetailsService.java
+++ src/main/java/com/mireilleProject/OnlineShopping/security/CustomUserDetailsService.java
@@ -1,4 +1,31 @@
 package com.mireilleProject.OnlineShopping.security;
 
-public class CustomUserDetailsService {
+import com.mireilleProject.OnlineShopping.entity.Admin;
+import com.mireilleProject.OnlineShopping.repositories.AdminRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private AdminRepository adminRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Admin admin = adminRepository.findAdminByEmail(email).orElseThrow(() -> new UsernameNotFoundException("user not exists by email"));
+
+        Set<GrantedAuthority> authorities = admin.getRoles().stream()
+                .map((role) -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toSet());
+        return new User(email, admin.getPassword(), authorities);
+    }
 }
