571932dae959649b57a4d6190aec5a76e06b0dd6
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-07-16 10:22:45 +0100

    Adde AdminRepositoty

--- src/main/java/com/mireilleProject/OnlineShopping/repositories/AdminRepository.java
+++ src/main/java/com/mireilleProject/OnlineShopping/repositories/AdminRepository.java
@@ -3,5 +3,10 @@ package com.mireilleProject.OnlineShopping.repositories;
 import com.mireilleProject.OnlineShopping.entity.Admin;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AdminRepository extends CrudRepository<Admin, Integer> {
+import java.util.Optional;
+
+public interface AdminRepository extends CrudRepository<Admin, Long> {
+
+    Optional<Admin> findAdminByEmail(String email);
+    Boolean existsByEmail(String email);
 }
