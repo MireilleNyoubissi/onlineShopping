3cf98a6275ca107cfa6ab7868f81a4d90346c08b
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-07-16 10:12:54 +0100

    Created PostMapping endpoints for register and login

--- src/main/java/com/mireilleProject/OnlineShopping/controller/AuthController.java
+++ src/main/java/com/mireilleProject/OnlineShopping/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.mireilleProject.OnlineShopping.controller;
 
-import com.mireilleProject.OnlineShopping.dto.registerDto;
-import com.mireilleProject.OnlineShopping.service.AdminService;
+import com.mireilleProject.OnlineShopping.dto.LoginDto;
+import com.mireilleProject.OnlineShopping.dto.RegisterDto;
+import com.mireilleProject.OnlineShopping.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,22 +12,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("api/")
-public class AdminController {
 
-    private AdminService adminService;
+//@AllArgsConstructor
+@RestController
+@RequestMapping("api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody registerDto registerDto) {
-        String response = adminService.registerAdmin(registerDto);
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
+        String response = authService.register(registerDto);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody registerDto registerDto){
-        String response = adminService.loginAdmin(registerDto);
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto){
+        String response = authService.login(loginDto);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
