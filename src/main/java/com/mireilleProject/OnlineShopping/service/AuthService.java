b06f200c4ca3183d2e786378b8fccbdb04a23438
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-07-16 09:57:23 +0100

    Changer AdminService to AuthService

--- src/main/java/com/mireilleProject/OnlineShopping/service/AuthService.java
+++ src/main/java/com/mireilleProject/OnlineShopping/service/AuthService.java
@@ -1,20 +1,18 @@
 package com.mireilleProject.OnlineShopping.service;
 
-import com.mireilleProject.OnlineShopping.dto.AdminDto;
+import com.mireilleProject.OnlineShopping.dto.LoginDto;
+import com.mireilleProject.OnlineShopping.dto.RegisterDto;
 import com.mireilleProject.OnlineShopping.entity.Admin;
-import com.mireilleProject.OnlineShopping.repositories.AdminRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
-@Service
-public interface AdminService {
+import java.util.Optional;
 
-    Admin findAdminByEmail(String email);
 
-    String registerAdmin(AdminDto adminDto);
+public interface AuthService {
 
-    String loginAdmin(AdminDto adminDto);
+    Optional<Admin> findAdminByEmail(String email);
+
+    String register(RegisterDto registerDto);
+
+    String login(LoginDto loginDto);
 
 }
