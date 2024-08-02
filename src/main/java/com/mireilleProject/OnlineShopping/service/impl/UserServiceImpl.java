d32917bb9b5b3d83456d3e3f891e6a6fb5dbfff7
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-08-02 03:14:58 +0100

    Added UserServiceimpl to implement modelMapper in order to convert entity into dto as well as dto to entity

--- src/main/java/com/mireilleProject/OnlineShopping/service/impl/UserServiceImpl.java
+++ src/main/java/com/mireilleProject/OnlineShopping/service/impl/UserServiceImpl.java
@@ -1,4 +1,30 @@
 package com.mireilleProject.OnlineShopping.service.impl;
 
-public class UserServiceImpl {
+import com.mireilleProject.OnlineShopping.dto.UserDto;
+import com.mireilleProject.OnlineShopping.entities.User;
+import com.mireilleProject.OnlineShopping.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private ModelMapper mapper;
+
+    //Convert entity into dto
+    private UserDto mapToDto(User user) {
+        UserDto userDto = mapper.map(user, UserDto.class);
+        return userDto;
+    }
+
+    //Convert dto to entity
+    private User mapToEntity(UserDto userDto) {
+        User user = mapper.map(userDto, User.class);
+        return user;
+    }
+
 }
