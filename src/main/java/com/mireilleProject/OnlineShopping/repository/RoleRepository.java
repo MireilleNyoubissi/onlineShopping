df3b229774f73449a283e7ffe7a8dd6c3296fa75
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-08-02 03:42:46 +0100

    Added Rolerepository interface Configure findByName method to find role by name

--- src/main/java/com/mireilleProject/OnlineShopping/repository/RoleRepository.java
+++ src/main/java/com/mireilleProject/OnlineShopping/repository/RoleRepository.java
@@ -1,4 +1,13 @@
 package com.mireilleProject.OnlineShopping.repository;
 
-public interface RoleRepository {
+import com.mireilleProject.OnlineShopping.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
 }
