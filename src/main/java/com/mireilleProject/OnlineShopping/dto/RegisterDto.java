7dd4d4755a308fe382734faad9cadeb2f03a9ed1
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-08-05 01:39:08 +0100

    Added a RegisterDto class

--- src/main/java/com/mireilleProject/OnlineShopping/dto/RegisterDto.java
+++ src/main/java/com/mireilleProject/OnlineShopping/dto/RegisterDto.java
@@ -1,4 +1,12 @@
 package com.mireilleProject.OnlineShopping.dto;
 
+import lombok.Data;
+
+@Data
 public class RegisterDto {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
 }
