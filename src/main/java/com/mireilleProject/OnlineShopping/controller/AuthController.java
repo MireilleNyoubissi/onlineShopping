1c49a6d50fbfdf89609e5ba5907f1fdd1f8819a8
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-08-05 01:42:23 +0100

    Added an AuthController class to implement PostMapping for register and login

--- src/main/java/com/mireilleProject/OnlineShopping/controller/AuthController.java
+++ src/main/java/com/mireilleProject/OnlineShopping/controller/AuthController.java
@@ -2,33 +2,32 @@ package com.mireilleProject.OnlineShopping.controller;
 
 import com.mireilleProject.OnlineShopping.dto.LoginDto;
 import com.mireilleProject.OnlineShopping.dto.RegisterDto;
-import com.mireilleProject.OnlineShopping.service.UserService;
+import com.mireilleProject.OnlineShopping.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
-public class UserController {
+@RequestMapping("/api/auth")
+public class AuthController {
     @Autowired
-    private UserService userService;
+    private AuthService authService;
 
     //Build register REST API
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
-        String response = userService.register(registerDto);
+        String response = authService.register(registerDto);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     //Build login REST API
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
-        String response = userService.login(loginDto);
+        String response = authService.login(loginDto);
         return ResponseEntity.ok(response);
     }
 
