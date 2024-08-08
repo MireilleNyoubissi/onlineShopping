407e181c1b7014d47f6d9a368f418c400af2cd48
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-08-08 07:49:36 +0100

    Added RoleInitializer class

--- src/main/java/com/mireilleProject/OnlineShopping/config/RoleInitializer.java
+++ src/main/java/com/mireilleProject/OnlineShopping/config/RoleInitializer.java
@@ -1,4 +1,23 @@
 package com.mireilleProject.OnlineShopping.config;
 
-public class RoleInitializer {
+import com.mireilleProject.OnlineShopping.entities.Role;
+import com.mireilleProject.OnlineShopping.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RoleInitializer implements CommandLineRunner {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (roleRepository.count() == 0) {
+            roleRepository.save(new Role("CUSTOMER"));
+            roleRepository.save(new Role("ADMIN"));
+
+        }
+    }
 }
